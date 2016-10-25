9b943da1a4b8bc0087cb3971a6d307cd9bd8de3b
Petersoj <jpet4781@740s-pl-170.local>
2016-10-25 13:15:25 -0600

    Added Card classes and Model and View folders

---
+++ CardGame/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Peterson, Jacob on 10/25/16.
+//  Copyright © 2016 Peterson, Jacob. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Peterson, Jacob on 10/25/16.
+//  Copyright © 2016 Peterson, Jacob. All rights reserved.
+//
+
+import Foundation
