ec70e281505c1a4a1c6bd358424872191787d9b7
Jacob Peterson <petersojj@gmail.com>
2016-11-09 12:07:48 -0700

    Added the shuffle deck method

--- CardGame/Deck.swift
+++ CardGame/Deck.swift
@@ -13,7 +13,13 @@ class Deck{
     internal lazy var cards = [Card]()
     
     func shuffleDeck() -> Void{
-        
+        var tempDeck = [Card]()
+        while self.cards.count > 0{
+            let randomIndex = Int(arc4random() % (UInt32)(cards.count))
+            let removedCard = cards.removeAtIndex(randomIndex)
+            tempDeck.append(removedCard)
+        }
+        self.cards = tempDeck
     }
 
     func cutDeck() -> Void{
